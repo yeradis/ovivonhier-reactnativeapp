90651625e0bff9b753f90ccae0259ab47c0bd4a6
Yeradis P. Barbosa Marrero <yeradis.barbosa@riseproject.com>
2018-10-07 09:21:45 +0200

    Minor update to code style

--- src/reducers/categoriesReducer.js
+++ src/reducers/categoriesReducer.js
@@ -1,12 +1,16 @@
-import {GET_CATEGORIES, GET_CATEGORIES_FULFILLED} from "../actions/types";
+import {
+    GET_CATEGORIES,
+    GET_CATEGORIES_FULFILLED
+} from '../actions/types';
 
 const categoriesReducer = (state = [], action) => {
-    switch(action.type) {
-        case GET_CATEGORIES, GET_CATEGORIES_FULFILLED:
-            return [...action.payload]
+    switch (action.type) {
+        case GET_CATEGORIES:
+        case GET_CATEGORIES_FULFILLED:
+            return [...action.payload];
         default:
             return state;
     }
-}
+};
 
 export default categoriesReducer;
--- src/reducers/categoryReducer.js
+++ src/reducers/categoryReducer.js
@@ -1,12 +1,16 @@
-import {GET_CATEGORY, GET_CATEGORY_FULFILLED} from "../actions/types";
+import {
+    GET_CATEGORY,
+    GET_CATEGORY_FULFILLED
+} from '../actions/types';
 
 const categoryReducer = (state = [], action) => {
-    switch(action.type) {
-        case GET_CATEGORY, GET_CATEGORY_FULFILLED:
-            return [...action.payload]
+    switch (action.type) {
+        case GET_CATEGORY:
+        case GET_CATEGORY_FULFILLED:
+            return [...action.payload];
         default:
             return state;
     }
-}
+};
 
 export default categoryReducer;
